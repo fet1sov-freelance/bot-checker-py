1a27537ba4307b7f1c926cebea7e70f6deea4d00
fet1sov <august9222@gmail.com>
2025-04-19 17:21:46 +0300

    fix(contact): contact import

--- utils.py
+++ utils.py
@@ -1,26 +1,42 @@
 from datetime import datetime
+from pyrogram.types import InputPhoneContact
+
+async def get_user_status_code(user_id, user):
+    if str(user.status) == "UserStatus.ONLINE":
+        print(f"User {user_id} is currently online")
+        return 1
+    elif str(user.status) == "UserStatus.OFFLINE":
+        now = datetime.now()
+        parsed_date = user.last_online_date
+
+        diff = now - parsed_date
+        minutes = diff.total_seconds() / 60
+
+        if minutes < 2:
+            print(f"User {user_id} was less than minute ago")
+            return 2
+        else:
+            print(f"User {user_id} is offline. Last seen: {user.last_online_date}")
+            return 0
 
 async def check_online_status(app, user_id):
         try:
-            user = await app.get_users(user_id)
+            if (user_id.startswith("+")):
+                print(f"Adding the {user_id} in the contacts")
 
-            if str(user.status) == "UserStatus.ONLINE":
-                print(f"User {user_id} is currently online")
-                return 1
-            elif str(user.status) == "UserStatus.OFFLINE":
-                now = datetime.now()
-                parsed_date = user.last_online_date
+                contact = await app.import_contacts([InputPhoneContact(user_id, user_id)])
 
-                diff = now - parsed_date
-                minutes = diff.total_seconds() / 60
-
-                if minutes < 2:
-                    print(f"User {user_id} was less than minute ago")
-                    return 2
-                else:
-                    print(f"User {user_id} is offline. Last seen: {user.last_online_date}")
-                    return 0
+                if contact.users:
+                    user = contact.users[0]
+                    if user:
+                        user_entity = await app.get_users(user.id)
+                        return await get_user_status_code(user_id, user_entity)
+                    else:
+                        print(f"{user_id} is not available to import")
+            else:
+                user = await app.get_users(user_id)
+                return await get_user_status_code(user_id, user)
 
         except Exception as e:
-            print(f"Error checking user status: {e}")
+            print(f"Error checking user status: {e.with_traceback()}")
             return 0
